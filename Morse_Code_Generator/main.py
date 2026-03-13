23def64caea5e233194c76fcc218bb96de286b67
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-03-13 20:02:52 +0530

    import json

--- Morse_Code_Generator/main.py
+++ Morse_Code_Generator/main.py
@@ -1,10 +1,17 @@
 from playsound3 import playsound
 import time
+import json
 
-sound = playsound('C:\\Users\\Shaur\\Desktop\\useful python\\Morse_Code_Generator\\Stay.mp3', block=False)
-while sound.is_alive():
-    print("Sound is still playing!")
-    time.sleep(2)
+# sound = playsound('C:\\Users\\Shaur\\Desktop\\useful python\\Morse_Code_Generator\\Stay.mp3', block=False)
+# while sound.is_alive():
+#     print("Sound is still playing!")
+#     time.sleep(2)
+
+with open("Morse_Code_Generator/morse_map.json", "r") as file:
+    morse_data = json.load(file)
+    print(morse_data)
+    
+    
     
     
 # sound.stop()
